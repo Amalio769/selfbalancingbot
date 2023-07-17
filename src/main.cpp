b7fe4ca370afb2983764fa94997d49435cacd92a
Amalio769 <amalio.rete-campos@airbus.com>
2023-07-17 17:17:20 +0200

    Added mpu6050 filtro complemento

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,13 @@
 MPU6050 sensor;
 
 // Valores RAW (sin procesar) del acelerometro y giroscopio en los ejes x,y,z
+int ax, ay, az;
 int gx, gy, gz;
 
-long tiempo_prev, dt;
-float girosc_ang_x, girosc_ang_y;
-float girosc_ang_x_prev, girosc_ang_y_prev;
+long tiempo_prev;
+float dt;
+float ang_x, ang_y;
+float ang_x_prev, ang_y_prev;
 
 void setup() {
   Serial.begin(9600);    //Iniciando puerto serial
@@ -22,31 +24,34 @@ void setup() {
 
   if (sensor.testConnection()) Serial.println("Sensor iniciado correctamente");
   else Serial.println("Error al iniciar el sensor");
-  tiempo_prev=millis();
 }
 
-
-
 void loop() {
-  // Leer las velocidades angulares 
+  // Leer las aceleraciones y velocidades angulares
+  sensor.getAcceleration(&ax, &ay, &az);
   sensor.getRotation(&gx, &gy, &gz);
   
-  //Calcular los angulos rotacion:
-  
-  dt = millis()-tiempo_prev;
+  dt = (millis()-tiempo_prev)/1000.0;
   tiempo_prev=millis();
   
-  girosc_ang_x = (gx/131)*dt/1000.0 + girosc_ang_x_prev;
-  girosc_ang_y = (gy/131)*dt/1000.0 + girosc_ang_y_prev;
-
-  girosc_ang_x_prev=girosc_ang_x;
-  girosc_ang_y_prev=girosc_ang_y;
+  //Calcular los ángulos con acelerometro
+  float accel_ang_x=atan(ay/sqrt(pow(ax,2) + pow(az,2)))*(180.0/3.14);
+  float accel_ang_y=atan(-ax/sqrt(pow(ay,2) + pow(az,2)))*(180.0/3.14);
+  
+  //Calcular angulo de rotación con giroscopio y filtro complemento  
+  ang_x = 0.98*(ang_x_prev+(gx/131)*dt) + 0.02*accel_ang_x;
+  ang_y = 0.98*(ang_y_prev+(gy/131)*dt) + 0.02*accel_ang_y;
+  
+  
+  ang_x_prev=ang_x;
+  ang_y_prev=ang_y;
 
   //Mostrar los angulos separadas por un [tab]
-  Serial.print("Rotacion en X:  ");
-  Serial.print(girosc_ang_x); 
-  Serial.print("tRotacion en Y: ");
-  Serial.println(girosc_ang_y);
 
-  delay(100);
+  Serial.print("Rotacion en X:  ");
+  Serial.print(ang_x); 
+  Serial.print("tRotacion en Y: ");
+  Serial.println(ang_y);
+
+  delay(10);
 }
